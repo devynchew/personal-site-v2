694c8b941d31c5524ab4a33b5cceddc37a8c3e5e
Devyn <devynchew@gmail.com>
2022-05-17 17:34:13 +0800

    Update new images, install staticimage plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
+// Add your plugins here!
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
