80e21c5752003c3b8e4d8b140c7c8ad622ea883b
Devyn <devynchew@gmail.com>
2022-05-18 12:19:38 +0800

    Add blog and page query

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,6 +23,7 @@ const Layout = ({ pageTitle, children }) => {
         <div className={container}>
             <title>{pageTitle} | {data.site.siteMetadata.title}</title>
             <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}>
@@ -33,6 +34,11 @@ const Layout = ({ pageTitle, children }) => {
                     <li className={navLinkItem}>
                         <Link to="/about" className={navLinkText}>
                             About
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>
+                            Blog
                         </Link>
                     </li>
                 </ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>I'm making this by following the Gatsby Tutorial. Hello</p>
       <StaticImage
         alt="Lighthouse"
         src="../images/lighthouse498x250.png"
