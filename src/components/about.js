8fa9abe5fe42807d5a9d9d27a05156785ad55ae8
Devyn <devynchew@gmail.com>
2022-05-20 14:07:44 +0800

    Add hover effects for buttons and links

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as about from './css/about.module.css'
+import { FaCarrot } from "@react-icons/all-files/fa/FaCarrot";
 
 const About = () => {
 
@@ -11,12 +12,12 @@ const About = () => {
                     <p>Hello! My name is Devyn and I love creating websites and web applications. I am an aspiring web developer from Singapore, currently studying Applied AI and Analytics at Singapore Polytechnic.</p>
                     <p>Here are a few technologies I've been working with recently:</p>
                     <ul className={about.skills}>
-                        <li>Javascript (ES6+)</li>
-                        <li>Prestashop</li>
-                        <li>React</li>
-                        <li>Gatsby</li>
-                        <li>Python</li>
-                        <li>Flask</li>
+                        <li><FaCarrot className={about.icon}/>Javascript (ES6+)</li>
+                        <li><FaCarrot className={about.icon}/>Prestashop</li>
+                        <li><FaCarrot className={about.icon}/>React</li>
+                        <li><FaCarrot className={about.icon}/>Gatsby</li>
+                        <li><FaCarrot className={about.icon}/>Python</li>
+                        <li><FaCarrot className={about.icon}/>Flask</li>
                     </ul>
                 </div>
                 <div></div>
