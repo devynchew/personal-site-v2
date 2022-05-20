4f437c20a3821e46d9c5d6a52db718d96b5f1425
Devyn <devynchew@gmail.com>
2022-05-20 15:17:08 +0800

    Add adjustments to letter, word spacing and added image to About Me section

--- src/components/about.js
+++ src/components/about.js
@@ -1,26 +1,32 @@
 import * as React from 'react'
 import * as about from './css/about.module.css'
 import { FaCarrot } from "@react-icons/all-files/fa/FaCarrot";
+import { StaticImage } from "gatsby-plugin-image"
 
 const About = () => {
 
     return (
         <section className={about.container}>
-            <h2>About Me</h2>
+            <h2 className={about.heading}>About Me</h2>
             <div className={about.column}>
                 <div className={about.leftColumn}>
                     <p>Hello! My name is Devyn and I love creating websites and web applications. I am an aspiring web developer from Singapore, currently studying Applied AI and Analytics at Singapore Polytechnic.</p>
+                    <p>My interest for coding started way back in 2017 when I saw a colleague using Microsoft Excel VBA to automate a spreadsheet report generator - that truly opened my eyes to the power of programming.</p>
                     <p>Here are a few technologies I've been working with recently:</p>
                     <ul className={about.skills}>
-                        <li><FaCarrot className={about.icon}/>Javascript (ES6+)</li>
-                        <li><FaCarrot className={about.icon}/>Prestashop</li>
-                        <li><FaCarrot className={about.icon}/>React</li>
-                        <li><FaCarrot className={about.icon}/>Gatsby</li>
-                        <li><FaCarrot className={about.icon}/>Python</li>
-                        <li><FaCarrot className={about.icon}/>Flask</li>
+                        <li><FaCarrot className={about.icon} />Javascript (ES6+)</li>
+                        <li><FaCarrot className={about.icon} />Prestashop</li>
+                        <li><FaCarrot className={about.icon} />React</li>
+                        <li><FaCarrot className={about.icon} />Gatsby</li>
+                        <li><FaCarrot className={about.icon} />Python</li>
+                        <li><FaCarrot className={about.icon} />Flask</li>
                     </ul>
                 </div>
-                <div></div>
+                <div className={about.rightColumn}>
+                    <div classname={about.photoWrapper}>
+                        <StaticImage src="../images/devyn.png" alt="Devyn's photo" />
+                    </div>
+                </div>
             </div>
         </section>
     )
