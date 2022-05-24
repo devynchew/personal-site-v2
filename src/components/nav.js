41423d5c1d65239d072ecc526e39d398d06cf234
Devyn <devynchew@gmail.com>
2022-05-24 17:49:46 +0800

    Add hamburger menu (incomplete)

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,19 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-
 import * as nav from './css/nav.module.css'
+import { GiHamburgerMenu } from "@react-icons/all-files/gi/GiHamburgerMenu";
+
 
 // pageTitle is a normal prop, children is a children prop
 const Nav = () => {
+    const [isNavExpanded, setIsNavExpanded] = React.useState(false)
 
     return (
         <header className={nav.header}>
             <nav className={nav.nav}>
                 <div>Logo
                 </div>
-                <div className={nav.navRight}>
+                <div className={isNavExpanded ? nav.navRightExpanded : nav.navRight}>
                     <ol className={nav.navLinks}>
                         <li className={nav.navLinkItem}>
                             <Link to="/#about" className={nav.navLinkText}>
@@ -29,12 +31,18 @@ const Nav = () => {
                             </Link>
                         </li>
                     </ol>
-                    <div>
+                    <div className={nav.resumeButtonWrapper}>
                         <Link to="/" className={nav.resumeButton}>
                             Resume
                         </Link>
                     </div>
                 </div>
+                {/* isNavExpanded will toggle between true and false whenever the icon is clicked. */}
+                <button className={nav.hamburger} onClick={() => {
+                    setIsNavExpanded(!isNavExpanded);
+                }}>
+                    <GiHamburgerMenu />
+                </button>
 
             </nav>
         </header>
