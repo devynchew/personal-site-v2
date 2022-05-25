57a195b30068ac645c79b3c6cd1bd38389278083
Devyn <devynchew@gmail.com>
2022-05-25 12:17:36 +0800

    Remove unnecessary files, fix contact email button

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
         <section id='contact' className={contact.container}>
             <h2 className={contact.heading}>Get In Touch</h2>
             <p className={contact.paragraph}>I'm looking for job opportunities and am more than happy to hear from you! Just drop me an email and I’ll try my best to get back to you.</p>
-            <a className={contact.button} to="mailto:devynchew@gmail.com">
+            <a className={contact.button} href="mailto:devynchew@gmail.com">
                 Say Hello
             </a>
         </section>
